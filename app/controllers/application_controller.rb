5354f9dfc7f1fd3a1d3550b2303d2d60b9f5ecc2
jacklin953107 <jacklin953107@gmail.com>
2018-01-15 08:29:44 +0800

    permit email of sign_up in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:password, :password_confirmation, :account, :name, :group, :rank])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :account, :name, :group, :rank])
     devise_parameter_sanitizer.permit(:account_update, keys: [:password, :password_confirmation, :account, :name, :group, :rank])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:account])
   end
